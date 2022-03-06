9b13526e43f933b809a8380fc2546d8fa9ded71c
isma4 <iscarina@unirioja.es>
2022-03-06 21:01:22 +0100

    Ejercicio 9

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,3 +1,9 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceID: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
